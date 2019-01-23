cf8e5fdeaed455abf3ef4b12b27f57af54cd5316
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-23 15:21:20 +0100

    Update database.js

--- Database/database.js
+++ Database/database.js
@@ -1,5 +1,17 @@
 var mysql = require('mysql');
 
+//DEFINE POOLING
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: "83.217.132.102",
+    port: '3306',
+    user: "root",
+    password: "Miroslava326356$$$$$",
+    database: "sampledb"
+});
+
+module.exports.pool = pool
+
 function CreatePool(db_loc) {
 
     return new Promise(function (resolve,reject) {
