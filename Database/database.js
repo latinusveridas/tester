aa5f036bd218f1236fe53e8fb5faeb2d9b244eed
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-23 17:19:20 +0100

    Update database.js

--- Database/database.js
+++ Database/database.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 //DEFINE POOLING
 var pool = mysql.createPool({
-    connectionLimit: 1,
+    connectionLimit: 30,
     host: "83.217.132.102",
     port: '3306',
     user: "root",
